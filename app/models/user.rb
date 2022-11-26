189e20daaddda6482250c613b613a1213dc6853f
fumi23232323 <ftfumift@gmail.com>
2022-11-26 21:02:18 +0900

    カテゴリー選択の設定(役職)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :department_category
+  belongs_to :position_category
   #空の投稿を保存できないようにする
   validates :email, :encrypted_password, :personnel_number, :department_category_id, :position_category_id, :last_name,:first_name, presence: true
 
