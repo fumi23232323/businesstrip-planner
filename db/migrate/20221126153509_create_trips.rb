d94a506e96b17587a7a14b8ef48a4fbb261adb77
fumi23232323 <ftfumift@gmail.com>
2022-11-28 23:26:25 +0900

    tripテーブルの修正

--- db/migrate/20221126153509_create_trips.rb
+++ db/migrate/20221126153509_create_trips.rb
@@ -5,13 +5,7 @@ class CreateTrips < ActiveRecord::Migration[6.0]
    t.string  :item_name,     null: false
    t.string  :first_day,     null: false
    t.integer :last_day,      null: false
-   t.integer :member_1_id
-   t.integer :member_2_id
-   t.string  :member_3_id
-   t.string  :member_4_id
-   t.string  :member_5_id
-
-      t.timestamps
+   t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,3 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_11_28_120838) do
+
+  create_table "trip_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "trip_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trip_id"], name: "index_trip_users_on_trip_id"
+    t.index ["user_id"], name: "index_trip_users_on_user_id"
+  end
+
+  create_table "trips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.string "first_day", null: false
+    t.integer "last_day", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "personnel_number", null: false
+    t.integer "department_category_id", null: false
+    t.integer "position_category_id", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "trip_users", "trips"
+  add_foreign_key "trip_users", "users"
+end
