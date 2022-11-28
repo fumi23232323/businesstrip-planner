b38dbde5e3263398c4ee750cb151df8dc1cc57d2
fumi23232323 <ftfumift@gmail.com>
2022-11-28 21:15:08 +0900

    カラムの修正（姓&名→姓名）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_11_28_120838) do
 
+  create_table "trip_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "trip_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trip_id"], name: "index_trip_users_on_trip_id"
+    t.index ["user_id"], name: "index_trip_users_on_user_id"
+  end
+
+  create_table "trips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.string "first_day", null: false
+    t.integer "last_day", null: false
+    t.integer "member_1_id"
+    t.integer "member_2_id"
+    t.string "member_3_id"
+    t.string "member_4_id"
+    t.string "member_5_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,8 +40,7 @@
     t.integer "personnel_number", null: false
     t.integer "department_category_id", null: false
     t.integer "position_category_id", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -29,4 +50,6 @@
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trip_users", "trips"
+  add_foreign_key "trip_users", "users"
 end
