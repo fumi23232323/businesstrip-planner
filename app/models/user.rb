8d80174d5555448b3a8af0f744580f64eb3147c7
fumi23232323 <ftfumift@gmail.com>
2022-12-07 12:43:18 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :trip_users
-         has_many :trips, through: :trip_users
+  has_many :trip_users
+  has_many :trips, through: :trip_users
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :department_category
