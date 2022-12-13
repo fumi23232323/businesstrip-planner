9ce4cdd4cff5500517b63ad0cbf685b33829c8b7
fumi23232323 <ftfumift@gmail.com>
2022-12-14 00:23:55 +0900

    ユーザー管理機能の実装_ログイン後にマイページへ遷移する設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
    # deviseコントローラー設定時
    before_action :configure_permitted_parameters, if: :devise_controller?
 
+   def after_sign_in_path_for(resource) 
+    trip_path(resource)
+ end
   private
 
   def basic_auth
