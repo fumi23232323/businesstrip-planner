5286adad1091d1110c0bbf3a0d2b2f051f51f089
fumi23232323 <ftfumift@gmail.com>
2022-12-15 12:47:15 +0900

    マイページの一覧の調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
+    @belong_trip_id = current_user.trips.ids
+        @trip = Trip.find([@belong_trip_id])
     @trips = user.trips
-    
   end
 end
